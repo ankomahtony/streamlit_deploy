86f190ad4f44e5ddaf726d1eab95e031058c7fba
Anthony Ankomah <tonyankomah1@gmail.com>
2021-03-22 19:12:58 +0000

    getting it ready to deploy

--- app.py
+++ app.py
@@ -6,27 +6,47 @@ import pandas as pd
 
 st.title('Deploying my first App')
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-'first column': [1, 2, 3, 4],
-'second column': [10, 20, 30, 40]
-}))
+# st.write("Here's our first attempt at using data to create a table:")
+# st.write(pd.DataFrame({
+# 'first column': [1, 2, 3, 4],
+# 'second column': [10, 20, 30, 40]
+# }))
 
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
+# """
+# # My first app
+# Here's our first attempt at using data to create a table:
+# """
 df = pd.DataFrame({
-'first column': [1, 2, 3, 4],
-'second column': [10, 20, 30, 40]
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
 })
-df
+# df
 
-chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+# st.line_chart(chart_data)
 
-map_data = pd.DataFrame(
-np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-columns=['lat', 'lon'])
-st.map(map_data)
+if st.sidebar.checkbox('Show Map'):
+    map_data = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+        columns=['lat', 'lon'])
+    st.map(map_data)
+
+if st.sidebar.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
+    st.line_chart(chart_data)
+
+option = st.sidebar.selectbox('Which number do you like best?', df['second column'])
+st.write('You selected: ', option)
+
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Woohoo!")
+    expander = st.beta_expander("FAQ")
+    expander.write("Here you could put in some really, really long explanations...")
+
+exp = st.beta_expander("Tag")
+exp = exp.write("Here be now...")
