3b32f45675f82868e22d467be1289defe701b8e2
Anthony Ankomah <tonyankomah1@gmail.com>
2021-03-23 11:33:34 +0000

    updating

--- app.py
+++ app.py
@@ -39,11 +39,11 @@ if st.checkbox('Show Chart'):
         st.line_chart(df_web[option])
 
 
-if st.sidebar.checkbox('Show Map'):
-    map_data = pd.DataFrame(
-        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-        columns=['lat', 'lon'])
-    st.map(map_data)
+# if st.sidebar.checkbox('Show Map'):
+#     map_data = pd.DataFrame(
+#         np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#         columns=['lat', 'lon'])
+#     st.map(map_data)
 
 
 left_column, right_column = st.beta_columns(2)
